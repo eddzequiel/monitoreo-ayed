d3892be8e80fb8ff89e86244a16c1bebef3b58b1
Eddy Moreno <eddymoreno10@gmail.com>
2019-03-13 09:07:02 -0600

    Update linked_list.cpp

--- unidad I/linked_list/linked_list.cpp
+++ unidad I/linked_list/linked_list.cpp
@@ -1,142 +1,118 @@
+#include <cstdlib>
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-
-struct nodo {
-	int dato;
-	struct nodo * siguiente;
-};
-
-
-void pausa()
+///Declaracion de la estructura
+typedef struct registro
 {
-    getchar();
-}
+    int elem;
+    struct registro *sig;
+} nodo;
 
+///Declaracion de las funciones a implementar
+nodo *nuevonodo (int info);
+void agregar (nodo **primero, int dato);
+void imprimir(nodo *primero);
+void borrar(nodo **primero, int dato);
 
-nodo *cabeza = NULL;
-
-
-// Devolviendo el puntero al elemento
-// cuyo dato es menor o igual al dato de entrada
-struct nodo *nodoBase(int n) { //Busca la estructura en memoria
-	if(cabeza == NULL) return cabeza;
-	nodo *actual = cabeza;
-	nodo *anterior = cabeza;
-	while(actual) {
-		if(actual->dato == n) return actual;
-		if(actual->dato > n) return anterior;
-		anterior = actual;
-		actual = actual->siguiente;
-	}
-}
-
-
-// Devolviendo el puntero al elemento
-// cuyo dato es igual al dato de entrada
-struct nodo *nodoBase2(int n) {
-	if(cabeza == NULL) return cabeza;
-	nodo *actual = cabeza;
-	nodo *anterior = cabeza;
-	while(actual) {
-		if(actual->dato == n) return actual;
-		anterior = actual;
-		actual = actual->siguiente;
-	}
-	return actual;
-}
-
-
-void agregarNodo(int n) {
-	nodo *nuevoNodo = new nodo;
-	nuevoNodo->dato = n;
-	nuevoNodo->siguiente = NULL;
-
-	if(cabeza == NULL) {
-		cabeza = nuevoNodo;
-		return;
-	}
-	nodo *actual = cabeza;
-	while(actual) {
-		if(actual->siguiente == NULL) {
-			actual->siguiente = nuevoNodo;
-			return;
-		}
-		actual = actual->siguiente;
-	}
-}
-
-
-void eliminarNodo(int n) {
-	nodo *puntero = nodoBase(n);
-	if(puntero == NULL) 
-		cout << "No existe el nodo con el valor = " << n << 
-endl;
-	if(puntero == cabeza) {
-		cabeza = puntero->siguiente;
-		return;
-	}
-	nodo *actual = cabeza;
-	nodo *anterior = cabeza;
-
-	while(actual) {
-		if(actual == puntero) {
-			anterior->siguiente = actual->siguiente;
-			return;
-		}
-		anterior = actual;
-		actual = actual->siguiente;
-	}
-}
-
-
-void imprimir() {
-	struct nodo *list = cabeza;
-	while(list) {
-		cout << list->dato <<" ";
-		list = list->siguiente;
-	}
-	cout << endl;
-}
-
-
-int main()
+///Proceso principal
+int main(int argc, char** argv)
 {
-    cout<<"\n--------------------------------------------------\n";
-	cout<<"-------------------Agregando nodo-------------------\n";
-	cout<<"\n--------------------------------------------------\n";
-	agregarNodo(10);
-    imprimir();
-    pausa();
-    
-	cout<<"\n--------------------------------------------------\n";
-	cout<<"-------------------Agregando nodo-------------------\n";
-	cout<<"\n--------------------------------------------------\n";
-	agregarNodo(20);
-	imprimir();
-	pausa();
-	
-	cout<<"\n--------------------------------------------------\n";
-	cout<<"-------------------Agregando nodo-------------------\n";
-	cout<<"\n--------------------------------------------------\n";
-	agregarNodo(30);
-	imprimir();
-	pausa();
-	
-	cout<<"\n--------------------------------------------------\n";
-	cout<<"-------------------Agregando nodo-------------------\n";
-	cout<<"\n--------------------------------------------------\n";
-	agregarNodo(40);
-	imprimir();
-	pausa();
-	
-	cout<<"\n--------------------------------------------------\n";
-	cout<<"-----------Eliminando nodo seleccionado-------------\n";
-	cout<<"\n--------------------------------------------------\n";
-	eliminarNodo(50);
-	imprimir();
-	pausa();
-	
-	return 0;
+    int x;
+    nodo *primero;
+    primero=NULL;
+    printf("Es momento de agregar valores a la lista :\n\n");
+    do
+    {
+        printf("Ingrese el dato a agregar (-1= Terminar): \n");
+        scanf("%d",&x);
+        if (x!=-1)
+            agregar(&primero,x);
+    } while(x!=-1);
+
+    imprimir(primero);
+
+    do
+    {
+        printf("Ingrese el dato a borrar (-1= Terminar): \n");
+        scanf("%d",&x);
+        if(x!=-1)
+            borrar(&primero,x);
+    } while(x!=-1);
+
+    imprimir(primero);
+
+    return 0;
 }
+
+///Funcion que crea un nodo
+void agregar(nodo **primero, int dato)
+{
+    nodo *nuevo;
+    nuevo=nuevonodo(dato);
+    if (*primero==NULL)
+        *primero=nuevo;
+    else
+    {
+        nuevo->sig=*primero;
+        *primero=nuevo;
+    }
+    return;
+}
+
+nodo *nuevonodo (int info)
+{
+    nodo *n;
+    n=(nodo*)malloc(sizeof(nodo));
+    n->sig=NULL;
+    n->elem=info;
+    return n;
+}
+
+///Funcion que borra los nodos
+void borrar(nodo **primero, int dato)
+{
+    nodo *anterior, *actual;
+    int encontrado=0;
+    anterior=NULL;
+    actual=*primero;
+
+    while((actual!=NULL) && (!encontrado))
+    {
+        encontrado=(actual->elem=dato);
+        if (!encontrado)
+        {
+            anterior=actual;
+            actual=actual->sig;
+        }
+    }
+    if(actual!=NULL)
+        if(actual==*primero)
+            *primero=actual->sig;
+        else
+            anterior->sig=actual->sig;
+
+    free(actual);
+
+    return;
+}
+
+///Funcion que imprime los nodos
+void imprimir (nodo *primero)
+{
+    printf("\n\n");
+
+    printf("La lista actual es:");
+    while(primero!=NULL)
+    {
+        printf("\t%d", primero->elem);
+        primero=(primero->sig);
+    }
+    printf("\n\n");
+
+    return;
+}
+
